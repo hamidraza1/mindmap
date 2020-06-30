192b1fb9f0110af2425f5467ec86ea403c7dd496
hamidraza1 <hrazaa306@gmail.com>
2020-06-30 22:49:44 +0200

    Updates

---
+++ precache-manifest.e743f4ac2b47fd4125fd3cb0ebba3cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fb902014f237cfe7362eb664386a83",
+    "url": "/mindmap/index.html"
+  },
+  {
+    "revision": "ade0c39cbb4df71a6488",
+    "url": "/mindmap/static/css/main.05f00f27.chunk.css"
+  },
+  {
+    "revision": "9473ed68785794bfee59",
+    "url": "/mindmap/static/js/2.894cf507.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mindmap/static/js/2.894cf507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade0c39cbb4df71a6488",
+    "url": "/mindmap/static/js/main.9792861d.chunk.js"
+  },
+  {
+    "revision": "bf08356ee4236c623981",
+    "url": "/mindmap/static/js/runtime-main.4435d2eb.js"
+  }
+]);
